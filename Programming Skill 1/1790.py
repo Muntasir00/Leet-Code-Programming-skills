b0c421e171ac9b9627df6da0bc13dc7f2ebfef43
Muntasir00 <muntasir690000@gmail.com>
2022-06-13 16:26:03 +0600

    programming skill 1 solved

--- Programming Skill 1/1790.py
+++ Programming Skill 1/1790.py
@@ -1,12 +1,13 @@
-def areAlmostEqual(s1: str, s2: str):
-    if list(s1) != list(s2): return False
-    mark = 0
-    for i in range(len(s1)):
-        if s1[i] != s2[i]: mark += 1
-        print(mark)
-        if mark > 2: 
-            print(mark)
-            return False
-    return True
-
-print(areAlmostEqual("abcc", "abca"))
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        if collections.Counter(s1)!=collections.Counter(s2):
+            return False 
+        if s1==s2:
+            return True 
+        
+        diff=0
+        for x,y in zip(s1,s2):
+            if x!=y:
+                diff+=1
+        return diff==2
+        
---
+++ Programming Skill 1/589.py
@@ -0,0 +1,13 @@
+class Solution:
+    def preorder(self,root:'Node')->List[int]:
+        output = []
+        def dfs(node):
+            if not node: return 
+            output.append(node.val)
+            
+            for c in node.children:
+                dfs(c)
+                
+        dfs(root)
+        return output
+        
