17c2bcee51822d29075a5176f8702563a46f31af
Muntasir00 <muntasir690000@gmail.com>
2022-09-13 22:26:45 +0600

    python practice

--- Solution with python/practice.py
+++ Solution with python/practice.py
@@ -1,14 +1,12 @@
-import random
-charsCU = list('ACDEFGHIJKLMNOPQRSTWXYZ')
-charscs = list('abcdefghijklmnopqrstwxyz')
-name = ""
-for x in range(1):
-    name += random.choice(charsCU)
-for x in range(10):
-    name += random.choice(charscs)
-name += " "
-for x in range(1):
-    name += random.choice(charsCU)
-for x in range(6):
-    name += random.choice(charscs)
-print(name)
+# import math
+# def isPrime(n):
+#     if(n<=1):
+#         return "not prime"
+#     for i in range(2,int(math.sqrt(n))+1):
+#         if(n%i==0):
+#             return "not prime"
+#     return "prime"
+
+# for i in range(30):
+#     print(i,"=",isPrime(i))   
+ 
